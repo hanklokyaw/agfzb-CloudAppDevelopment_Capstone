cc379f2fd836a58c06f2f19103dc775547c63a0d
hanklokyaw <hank.lo.kyaw@gmail.com>
2022-03-30 15:46:25 +0000

    fixed review bug

--- server/djangobackend/urls.py
+++ server/djangobackend/urls.py
@@ -20,5 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('djangoapp/', include('djangobackend.urls')),
     path('djangoapp/', include('djangoapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
